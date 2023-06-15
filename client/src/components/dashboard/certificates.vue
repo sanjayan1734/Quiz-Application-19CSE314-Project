30829d0b4320b19655682f569263017f988b0b1f
Harish Sankaranarayanan S <harish251102@gmail.com>
2023-06-16 03:17:05 +0530

    added images

--- client/src/components/dashboard/certificates.vue
+++ client/src/components/dashboard/certificates.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="certification-page">
       <h2 class="certification-title">Quiz Certification</h2>
-      <div class="certification-content">
+      <p class="certification-note">Note: The following certificates are sample certificates.</p>
+      <div v-for="quiz in quizzes" :key="quiz.id" class="certification-card">
         <div class="certification-details">
           <div class="certification-item">
             <span class="certification-label">Quiz Title:</span>
-            <span class="certification-value">{{ quizTitle }}</span>
+            <span class="certification-value">{{ quiz.title }}</span>
           </div>
           <div class="certification-item">
             <span class="certification-label">Date Completed:</span>
-            <span class="certification-value">{{ completionDate }}</span>
+            <span class="certification-value">{{ quiz.completionDate }}</span>
           </div>
           <div class="certification-item">
             <span class="certification-label">Score:</span>
-            <span class="certification-value">{{ score }}%</span>
+            <span class="certification-value">{{ quiz.score }}%</span>
           </div>
-        </div>
-        <div class="certificate-container">
-          <img :src="certificateImage" class="certificate-image" alt="Certificate" />
-          <button @click="downloadCertificate" class="download-button">Download Certificate</button>
+          <div class="certification-item">
+            <span class="certification-label">Issuer:</span>
+            <span class="certification-value">{{ quiz.issuer }}</span>
+          </div>
+          <div class="certificate-image">
+  <img src="quiz.certificateImageUrl" alt="Certificate" class="certificate-img">
+</div>
+          <div class="certification-item">
+            <span class="certification-label">Certificate:</span>
+            <button @click="downloadCertificate(quiz.certificateUrl)" class="download-button">Download Certificate</button>
+          </div>
         </div>
       </div>
     </div>
@@ -28,26 +36,44 @@
   export default {
     data() {
       return {
-        certificateImage: 'quiz1.pdf', // Path to the certificate image for the current quiz
-        quizTitle: '', // Title of the quiz
-        completionDate: '', // Date when the quiz was completed
-        score: 0 // Score achieved in the quiz
+        quizzes: [
+          {
+            id: 1,
+            title: 'Quiz 1',
+            completionDate: 'June 15, 2023',
+            score: 90,
+            issuer: 'Sample Issuer',
+            certificateImageUrl:'https://drive.google.com/file/d/1ObANxb6go-1zBtvebKd1LIKwcJjidTu7/view?usp=sharing',
+            certificateUrl: 'https://drive.google.com/file/d/1eCFGBefHzYGfuiBqaxPAjWOtxPyMfSJj/view?usp=sharing'
+          },
+          {
+            id: 2,
+            title: 'Quiz 2',
+            completionDate: 'June 16, 2023',
+            score: 85,
+            issuer: 'Sample Issuer',
+            certificateImageUrl:'quiz2.png',
+            certificateUrl: 'https://drive.google.com/file/d/1I910j85WG3z7pEqyh-5_S1IMRnkHcvJ6/view?usp=sharing'
+          },
+          {
+            id: 3,
+            title: 'Quiz 3',
+            completionDate: 'June 13, 2023',
+            score: 95,
+            issuer: 'Sample Issuer',
+            certificateImageUrl:'quiz3.png',
+            certificateUrl: 'https://drive.google.com/file/d/1ImocrdfmO5NRpcnZN8OwxjVCI7pDsnVX/view?usp=sharing'
+          }
+        ]
       };
     },
     methods: {
-      downloadCertificate() {
+      downloadCertificate(certificateUrl) {
         const link = document.createElement('a');
-        link.href = this.certificateImage;
-        link.download = 'certificate.png';
+        link.href = certificateUrl;
+        link.download = 'certificate.pdf';
         link.click();
       }
-    },
-    mounted() {
-      // Fetch and set the quiz details and certificate image based on the selected quiz
-      this.quizTitle = 'Sample Quiz';
-      this.completionDate = 'June 15, 2023';
-      this.score = 90;
-      this.certificateImage = '/path/to/certificate-image.png';
     }
   };
   </script>
@@ -65,62 +91,66 @@
     font-size: 24px;
     font-weight: bold;
     margin-bottom: 20px;
+    color: #333;
   }
   
-  .certification-content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  .certification-note {
+    margin-bottom: 20px;
+    color: #777;
+  }
+  
+  .certification-card {
     width: 100%;
     max-width: 800px;
-    margin: 0 auto;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 20px;
   }
   
   .certification-details {
-    margin-top: 20px;
-    border: 1px solid #ccc;
-    padding: 20px;
-    border-radius: 4px;
-    width: 100%;
+    display: flex;
+    flex-direction: column;
   }
   
   .certification-item {
     display: flex;
     justify-content: space-between;
     margin-bottom: 10px;
+    color: #333;
   }
   
   .certification-label {
     font-weight: bold;
   }
   
-  .certificate-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 30px;
+  .certification-value {
+    margin-left: 10px;
   }
   
   .certificate-image {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+  }
+  
+  .certificate-img {
     max-width: 100%;
     height: auto;
-    margin-bottom: 20px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   }
   
   .download-button {
-    margin-top: 20px;
-    padding: 10px 20px;
+    padding: 5px 10px;
     background-color: #007bff;
     color: #fff;
     border: none;
     border-radius: 4px;
-    font-size: 16px;
     cursor: pointer;
+    font-size: 14px;
   }
   
   @media (max-width: 768px) {
-    .certification-details {
+    .certification-card {
       font-size: 14px;
     }
   }
